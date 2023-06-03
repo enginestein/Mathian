d7ab506c859983630c18a6e274b124d436160440
Arya Praneil Pritesh <117010357+aryapraneil@users.noreply.github.com>
2023-06-03 14:42:51 +0530

    Delete x64/x64/asm directory

--- x64/x64/asm/beforefieldinit.asm
+++
@@ -1,4 +0,0 @@
-.class /*02000002*/ public auto ansi beforefieldinit 'Sigmath'.'ArithmeticProgression'
-       extends ['mscorlib'/*23000001*/]'System'.'Object'/*0100000F*/
-{
-} // end of class 'Sigmath'.'ArithmeticProgression'
--- x64/x64/asm/init.asm
+++
@@ -1,4 +0,0 @@
-.class /*02000006*/ public auto ansi beforefieldinit 'Sigmath'.'Basic'
-       extends ['mscorlib'/*23000001*/]'System'.'Object'/*0100000F*/
-{
-} // end of class 'Sigmath'.'Basic'
